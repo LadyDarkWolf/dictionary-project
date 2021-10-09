0a4eff91e87c14224f4c7b1e2f84663e07a89c90
LadyDarkWolf <87519139+LadyDarkWolf@users.noreply.github.com>
2021-10-09 19:35:34 +1100

    Added search form and handling.  Minor fiddling with layout.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
+      <div className="container">
+        <header className="App-header">
+          <h1>Testing</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by Lisa Wolf. Open sourced with source code at{" "}
+          <a
+            href="https://github.com/LadyDarkWolf/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
